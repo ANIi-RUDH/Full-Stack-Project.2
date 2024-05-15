98869c93aeeeafc90b61e75321401a942f8a03b9
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2024-05-15 11:24:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todoDB");
+mongoose.connect("#######");
 
 const itemSchema={
   name:String, 
